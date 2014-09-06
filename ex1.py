1252ee470e8965d2d0c6f6624e0a7af6a5e50df0
Payam Rastogi <payamrastogi@gmail.com>
2014-09-06 15:02:12 -0400

    exercises

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "hello world!"
+print "helLO aGAIN"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#print "Payam Rastogi"
