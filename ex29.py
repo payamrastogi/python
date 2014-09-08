e272d83f4505d34eaac0343b44e27a24f7ffbc14
Payam Rastogi <payamrastogi@gmail.com>
2014-09-07 23:31:23 -0400

    if -else

---
+++ ex29.py
@@ -0,0 +1,14 @@
+x = 20
+y = 30
+
+
+if x < y :
+	print "x < y"
+if x > y :
+ print "x > y"
+ 
+if True :
+	print "true"
+
+if False:
+	print "false"
---
+++ ex30.py
@@ -0,0 +1,22 @@
+x = 20
+y = 30
+
+
+if x < y :
+	print "x < y"
+elif x > y :
+	print "x > y"
+else:
+	print "x==y"
+ 
+if True :
+	print "true"
+elif True:
+	print "true1"
+elif True:
+	print "true2"
+else :
+	print "True3"
+
+if False:
+	print "false"
